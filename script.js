2634731cc2fdbf6d1f0b7acdfe68724ae6509d2b
Astha Jyoti <asthajyoti1075@gmail.com>
2020-07-11 20:48:10 +0530

    commit

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 var board;
+var player;
 const human='O';
-const ai='X';
+const opponent='X';
+var ai;
+var friend;
+var temp;
 const wins=[
     [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 ]
 const cells=document.querySelectorAll(".cell");
 startGame();
+//h2h();
 
+function opposite1(ai) {
+    temp='ai';
+        ai=opponent;
+        
+        //console.log(temp);
+        
+        document.getElementById("ai").style.backgroundColor="green";
+        //console.log(ai);
+        document.getElementById("friend").style.backgroundColor="rgb(248, 85, 166)";
+        
+    }
+function opposite(friend)    {
+    
+        friend=opponent;
+        temp='friend';
+        document.getElementById("friend").style.backgroundColor="green";
+        document.getElementById("ai").style.backgroundColor="rgb(248, 85, 166)";
+    
+}
+/*function h2h()
+{
+    document.querySelector(".endgame").style.display="none";
+    board=Array.from(Array(9).keys());
+    for(var i=0;i<cells.length;i++)
+    {
+        cells[i].innerText="";
+        cells[i].style.removeProperty("background-color");
+        cells[i].addEventListener('click',turnClick,false);
+    }
+}
+function turnClick(square) {
+    if(typeof board[square.target.id]=="number") {
+    turn(square.target.id,human);
+    if(!checkWin(board,human)&&!checkTie()) {
+        turn(square.target.id, friend);
+    }
+}
+}
+function start() {
+    if(temp==='ai') {
+        h2h();
+    }
+    else if(temp==='friend') {
+        startGame();
+    }
+}*/
 
 function startGame() {
     document.querySelector(".endgame").style.display="none";
@@ -20,11 +71,28 @@ function startGame() {
 }
 function turnclick(square) {
     if(typeof board[square.target.id]=="number") {
-    turn(square.target.id,human)
-    if(!checkWin(board,human)&&!checkTie()) turn(bestSpot(), ai);
-    
+    turn(square.target.id,human);
+    if(!checkWin(board,human)&&!checkTie()) {
+        //if(temp===ai) 
+        turn(bestSpot(), ai);
+        //else if(temp===friend) 
+       // {turn(square.target.id, friend); 
+        //console.log(friend);}
 }
 }
+}
+
+/*function checkMove(temp,square) {
+    if(temp===ai)
+    {
+        turn(bestSpot(), ai); 
+    }
+    else if(temp===friend)
+    {
+        turn(square, friend); 
+    }
+}*/
+
 function turn(squareId, player) {
     board[squareId]=player;
     document.getElementById(squareId).innerText=player;
@@ -60,11 +128,11 @@ document.querySelector(".endgame .text").innerText=who;
 }
 
 function empty() {
-    return board.filter(s => typeof s == "number");
+    return board.filter(s => typeof s == 'number');
 }
 
 function bestSpot() {
-    return (minimax(board, ai)).index;
+    return minimax(board, ai).index;
 }
 function checkTie()
 {
@@ -85,33 +153,39 @@ function checkTie()
 
 //console.log(empty());
 
-function minimax(newBoard,player) {
+function minimax(board,player) {
     var availSpots=empty();
-    if(checkWin(newBoard,human)) {
+    if(checkWin(board,human)) {
     return {score: -10};
-     } else if(checkWin(newBoard,ai)) {
+     } else if(checkWin(board,ai)) {
     return {score: 10};
      } else if(availSpots===0) {
     return {score: 0};
      }
      
      var moves=[];
-     for(var i=0;i<availSpots.length;i++)
+     for(let i=0;i<availSpots.length;i++)
      {
-         var move={};     
-    move.index=newBoard[availSpots[i]];
-    newBoard[availSpots[i]]=player;
-    if(player===ai) {
-        var result=minimax(newBoard,human);
-        move.score=result.score;
-        
+         var move={}; 
+         let id=availSpots[i];
+         let backUp=board[id];
+         board[id]=player;    
+         move.id=id;
+    //move.index=newBoard[availSpots[i]];
+    //newBoard[availSpots[i]]=player;
+    if(player==ai) {
+        move.score=minimax(board,human).score;
     } else {
-        var result=minimax(newBoard,ai);
-        move.score=result.score;
+        //var result=minimax(newBoard,ai);
+        move.score=minimax(board,ai).score;
         //move.score=(minimax(newBoard,ai)).score;
     }
-    newBoard[availSpots[i]]=move.index;
-    moves.push(move);
+    board[id]=backUp;
+   /* if ((player === ai && move.score === 10) || (player === human && move.score === -10))
+      return move;
+    else */
+      moves.push(move);
+    
 }
 var bestMove;
 if(player===ai) {
@@ -119,7 +193,7 @@ if(player===ai) {
     for (var i = 0; i < moves.length; i++) {
         if(moves[i].score>bestScore) {
             bestScore=moves[i].score;
-            bestMove=i;
+            bestMove=moves[i];
         }
         
     }
@@ -134,7 +208,7 @@ if(player===ai) {
         
     }
  }
-return moves[bestMove];
+return bestMove;
 }
 /*function gameScore(mini_max,newBoard,availSpots) {
     if(minimax(newBoard,human)&&checkWin(newBoard,human)) {
